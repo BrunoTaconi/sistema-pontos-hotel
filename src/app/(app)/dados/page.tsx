b0b18e673119b77dd1bf99ebf22f8f5258273956
gboechatdf <gboechatdf@gmail.com>
2025-09-05 20:15:51 -0300

    criação pagina meus dados

--- src/app/(app)/dados/page.tsx
+++ src/app/(app)/dados/page.tsx
@@ -1,19 +1,22 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
 import styles from "./styles.module.css";
 import Image from "next/image";
-import { FaUser, FaEnvelope, FaMapMarkerAlt, FaPhone } from "react-icons/fa";
+import { FaUser, FaArrowLeft, FaEnvelope, FaMapMarkerAlt, FaPhone, FaFolderMinus, FaFolder, FaIdCard } from "react-icons/fa";
+import { FaFolderTree, FaIdBadge } from "react-icons/fa6";
 
 interface UserData {
   nome: string;
   email: string;
   telefone?: string;
   numeroDocumento?: string;
-  /* fotoPerfil?: string; // Add a property for the profile picture URL*/
+  saldoPontos: number; // Adicionando o saldo de pontos
 }
 
 const MyDataPage = () => {
+  const router = useRouter();
   const [userData, setUserData] = useState<UserData | null>(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
@@ -26,6 +29,7 @@ const MyDataPage = () => {
           throw new Error("Não foi possível carregar os dados do usuário.");
         }
         const data: UserData = await res.json();
+        // Simulação de dados para exemplo
         setUserData(data);
       } catch (err) {
         setError(err instanceof Error ? err.message : "Ocorreu um erro desconhecido.");
@@ -38,62 +42,87 @@ const MyDataPage = () => {
   }, []);
 
   if (loading) {
-    return <div className={styles.container}>Carregando seus dados...</div>;
+    return (
+      <div className={styles.container}>
+        <div className={styles.pageContent}>
+          <p>Carregando seus dados...</p>
+        </div>
+      </div>
+    );
   }
 
   if (error) {
-    return <div className={styles.container}>Erro: {error}</div>;
+    return (
+      <div className={styles.container}>
+        <div className={styles.pageContent}>
+          <p>Erro: {error}</p>
+        </div>
+      </div>
+    );
   }
 
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>Meus Dados</h1>
-      
-
-      {userData && (
-        <div className={styles.cardsWrapper}>
-          {/* Card 1: Profile Picture and Name */}
-          <div className={`${styles.card} ${styles.profileCard}`}>
-            <Image
-              src={"/standard_profilepic.png"}
-              alt="Foto de Perfil"
-              width={100}
-              height={100}
-              className={styles.profileImage}
-            />
-            <h2>{userData.nome}</h2>
-          </div>
-
-          {/* Card 2: Other Information */}
-          <div className={styles.card}>
-            <div className={styles.dataRow}>
-              <FaEnvelope size={20} className={styles.icon} />
-              <div className={styles.dataText}>
-                <span className={styles.label}>Email:</span>
-                <p>{userData.email}</p>
-              </div>
-            </div>
-            {userData.telefone && (
-              <div className={styles.dataRow}>
-                <FaPhone style={{ transform: "rotate(90deg)" }} size={20} fa-rotate-180 className={styles.icon} />
-                <div className={styles.dataText}>
-                  <span className={styles.label}>Telefone:</span>
-                  <p>{userData.telefone}</p>
-                </div>
-              </div>
-            )}
-            {userData.numeroDocumento && (
-              <div className={styles.dataRow}>
-                <FaMapMarkerAlt size={20} className={styles.icon} />
-                <div className={styles.dataText}>
-                  <span className={styles.label}>Documento:</span>
-                  <p>{userData.numeroDocumento}</p>
-                </div>
-              </div>
-            )}
-          </div>
+      <div className={styles.pageContent}>
+        {/* Este é o cabeçalho com o botão e o título */}
+        <div className={styles.backContainer}>
+          <button onClick={() => router.back()} className={styles.backButton}>
+            <FaArrowLeft size={17} />
+          </button>
+          <p>Meus Dados</p>
         </div>
-      )}
+
+        {userData && (
+          <div className={styles.cardsWrapper}>
+            {/* Card de Perfil (vertical) */}
+            <div className={`${styles.card} ${styles.profileCard}`}>
+              { <Image
+                src={"/standard_profilepic.png"}
+                alt="Foto de Perfil"
+                width={100}
+                height={100}
+                className={styles.profileImage}
+              /> }
+              <h2>{userData.nome}</h2>
+              {/* Adicionando o total de pontos */}
+              <div className={styles.pointsTotal}>
+                <span>Total de Pontos:</span>
+                <p>{userData.saldoPontos} Real Points</p>
+              </div>
+              
+            </div>
+
+            {/* Card de Dados (vertical) */}
+            <div className={styles.card}>
+              <div className={styles.dataRow}>
+                <FaEnvelope size={20} className={styles.icon} />
+                <div className={styles.dataText}>
+                  <span className={styles.label}>Email:</span>
+                  <p>{userData.email}</p>
+                </div>
+              </div>
+              {userData.telefone && (
+                <div className={styles.dataRow}>
+                  <FaPhone style={{ transform: "rotate(90deg)" }} size={20} className={styles.icon} />
+                  <div className={styles.dataText}>
+                    <span className={styles.label}>Telefone:</span>
+                    <p>{userData.telefone}</p>
+                  </div>
+                </div>
+              )}
+              {userData.numeroDocumento && (
+                <div className={styles.dataRow}>
+                  <FaIdCard size={20} className={styles.icon} />
+                  <div className={styles.dataText}>
+                    <span className={styles.label}>Documento:</span>
+                    <p>{userData.numeroDocumento}</p>
+                  </div>
+                </div>
+              )}
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
